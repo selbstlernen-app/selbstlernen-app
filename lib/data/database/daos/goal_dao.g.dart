2a9b4c6d273352ae11a8ff0ebc3d28a5438e289b
Madeleine <mdormeye@smail.uni-koeln.de>
2025-10-23 11:51:29 +0200

    Removed all files that are in the .gitignore

--- lib/data/database/daos/goal_dao.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'goal_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$GoalDaoMixin on DatabaseAccessor<AppDatabase> {
-  $SessionsTable get sessions => attachedDatabase.sessions;
-  $GoalsTable get goals => attachedDatabase.goals;
-}
--- lib/data/database/daos/session_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'session_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$SessionDaoMixin on DatabaseAccessor<AppDatabase> {
-  $SessionsTable get sessions => attachedDatabase.sessions;
-}
