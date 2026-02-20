883db259c43982f9099624e4db897d687e40a2a0
Madeleine <mdormeye@smail.uni-koeln.de>
2026-02-20 13:37:29 +0100

    rm import

--- lib/presentation/screens/add_session/pages/goal_setting_page.dart
+++ lib/presentation/screens/add_session/pages/goal_setting_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:srl_app/common_widgets/common_widgets.dart';
 import 'package:srl_app/common_widgets/custom_add_item_field.dart';
-import 'package:srl_app/common_widgets/custom_error_text.dart';
 import 'package:srl_app/common_widgets/spacing/spacing.dart';
 import 'package:srl_app/core/utils/build_context_extensions.dart';
 import 'package:srl_app/domain/models/models.dart';
